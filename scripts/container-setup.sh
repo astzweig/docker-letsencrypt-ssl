0c1c75380db420a7902887925e18a732af5ba762
Rezart Qelibari <it@astzweig.de>
2018-09-02 16:00:11 +0200

    Download lexicon repo

--- scripts/container-setup.sh
+++ scripts/container-setup.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+_grab_repo() {
+    local DOWNLOAD_URL="${1}";
+    local DOWNLOAD_FOLDER="${2}";
+    local BRANCH_NAME="${3}";
+    if [ -d "${DOWNLOAD_FOLDER}/.git" ]; then
+        return 0;
+    fi
+    git clone "${DOWNLOAD_URL}" "${DOWNLOAD_FOLDER}";
+    [ ! -d "${DOWNLOAD_FOLDER}" ] && exit 1;
+
+    if [ "${BRANCH_NAME}" != 'master' ]; then
+        cd "${DOWNLOAD_FOLDER}";
+        git checkout "${BRANCH_NAME}";
+    fi
+}
+
 main () {
     local GITHUB_SLUG="${GITHUB_SLUG:-analogj/lexicon}";
     local BRANCH="${GITHUB_BRANCH:-master}";
     local PROVIDER="${PROVIDER}";
     local REPO_DIR="/lexicon-repo";
+
+    _grab_repo "https://github.com/${GITHUB_SLUG}.git" \
+               "${REPO_DIR}" \
+               "${BRANCH}";
     return 0;
 }
 
