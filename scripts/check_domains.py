fd84b631df7287583620f392cc32dbc6d3df3aa8
Rezart Qelibari <it@astzweig.de>
2020-06-09 15:15:32 +0200

    Use sys.argv as list

--- scripts/check_domains.py
+++ scripts/check_domains.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, '/usr/local/bin')
 from hook import Storage, STORAGE_PATH
 
-domains = sys.argv(1).split(',')
+domains = sys.argv[1].split(',')
 for domain in domains:
     storage = Storage(STORAGE_PATH)
     if not storage.fetch(DOMAIN):
