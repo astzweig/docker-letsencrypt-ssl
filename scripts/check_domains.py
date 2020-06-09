8eb14d0dfa986332bc84f3e6e841db422bfb7c15
Rezart Qelibari <it@astzweig.de>
2020-06-09 15:16:56 +0200

    Replace DOMAINS with local variable

--- scripts/check_domains.py
+++ scripts/check_domains.py
@@ -7,7 +7,7 @@ from hook import Storage, STORAGE_PATH
 domains = sys.argv[1].split(',')
 for domain in domains:
     storage = Storage(STORAGE_PATH)
-    if not storage.fetch(DOMAIN):
+    if not storage.fetch(domain):
         print('--debug-challenge', end="")
         sys.exit(0)
 print('--non-interactive', end="")
